bdaa5e91bab42bc9f6413b0ee78303e286c1aa72
lgiglioli <leandro.giglioli@gmail.com>
2024-10-08 22:40:33 -0400

    Fixed; lint

--- src/gbfs_analytics/__init__.py
+++ src/gbfs_analytics/__init__.py
@@ -1,7 +1,8 @@
 import logging
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.info('Package initialized!')
+logger.info("Package initialized!")
 
 __version__ = "0.1.0"
 
@@ -9,4 +10,4 @@ from .gbfs_manager import GBFSManager
 from .gbfs_feed import gbfs_feed
 from .gbfs_timeseries import GBFSTimeSeries
 
-__all__ = ['GBFSManager', 'gbfs_feed', 'GBFSTimeSeries']
+__all__ = ["GBFSManager", "gbfs_feed", "GBFSTimeSeries"]
--- src/gbfs_analytics/gbfs_manager.py
+++ src/gbfs_analytics/gbfs_manager.py
@@ -1,29 +1,32 @@
+# gbfs_manager.py
+
+# local modules
 from .gbfs_feed import gbfs_feed
+
 
 class GBFSManager:
     def __init__(self):
-        #available systems and their base URLs
+        # available systems and their base URLs
         self.systems = {
-            'dc': 'https://gbfs.capitalbikeshare.com/gbfs/2.3/gbfs.json',
-            'nyc': 'https://gbfs.lyft.com/gbfs/2.3/bkn/gbfs.json',
-            'boston': 'https://gbfs.bluebikes.com/gbfs/gbfs.json',
-            'chicago': 'https://gbfs.divvybikes.com/gbfs/2.3/gbfs.json',
-            'sf': 'https://gbfs.baywheels.com/gbfs/2.3/gbfs.json',
-            'portland': 'https://gbfs.biketownpdx.com/gbfs/2.3/gbfs.json',
-            'denver': 'https://gbfs.lyft.com/gbfs/2.3/den/gbfs.json',
-            'columbus': 'https://gbfs.lyft.com/gbfs/2.3/cmh/gbfs.json',
-            'la': 'https://gbfs.bcycle.com/bcycle_lametro/gbfs.json',
-            'phila': 'https://gbfs.bcycle.com/bcycle_indego/gbfs.json',
-            'toronto': 'https://tor.publicbikesystem.net/customer/gbfs/v2/gbfs.json'
+            "dc": "https://gbfs.capitalbikeshare.com/gbfs/2.3/gbfs.json",
+            "nyc": "https://gbfs.lyft.com/gbfs/2.3/bkn/gbfs.json",
+            "boston": "https://gbfs.bluebikes.com/gbfs/gbfs.json",
+            "chicago": "https://gbfs.divvybikes.com/gbfs/2.3/gbfs.json",
+            "sf": "https://gbfs.baywheels.com/gbfs/2.3/gbfs.json",
+            "portland": "https://gbfs.biketownpdx.com/gbfs/2.3/gbfs.json",
+            "denver": "https://gbfs.lyft.com/gbfs/2.3/den/gbfs.json",
+            "columbus": "https://gbfs.lyft.com/gbfs/2.3/cmh/gbfs.json",
+            "la": "https://gbfs.bcycle.com/bcycle_lametro/gbfs.json",
+            "phila": "https://gbfs.bcycle.com/bcycle_indego/gbfs.json",
+            "toronto": "https://tor.publicbikesystem.net/customer/gbfs/v2/gbfs.json",
         }
 
     def get_feed(self, city):
         """Return a gbfs_feed instance for the given city if available."""
         if city not in self.systems:
-            raise ValueError(f"City '{city}' is not available in the systems list.")
+            raise ValueError(f"City {city} is not available in the systems list.")
         return gbfs_feed(city, self.systems[city])
-    
+
     def list_available_systems(self):
         """Return a list of available systems."""
         return list(self.systems.keys())
-
